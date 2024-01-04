5f6f988d5a67f3a84434f62cea65c2d5df1f1539
Abu Bakar Tahir <ch.abu.bakar.tahir.zain@gmail.com>
2024-01-04 21:06:11 +0500

    Clicky Crates Microgame
    A game I recreated during my  learning process from Unity Junior Programmer Pathway. Will add some more things to it in future just like all other projects related to unity Junior Programmer Pathway.

---
+++ Course-Practice-Projects/Clicky Crates/Assets/Scripts/GameManager.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+    public List<GameObject> Targets;
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI gameOverText;
+    public Button restartButton;
+    public GameObject titleScreen;
+    public bool isGameActive;
+    private float spawnRate = 1.0f;
+    private int score;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void StartGame(int difficulty)
+    {
+        isGameActive=true;
+        spawnRate /= difficulty;
+        score = 0; 
+        titleScreen.gameObject.SetActive(false);
+        StartCoroutine(SpawnTarget());
+        UpdateScore(0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator SpawnTarget()
+    {
+        while (isGameActive)
+        {
+            yield return new WaitForSeconds(spawnRate);
+            int index = Random.Range(0, Targets.Count);
+            Instantiate(Targets[index]);
+        }
+    }
+
+    public void UpdateScore(int scoreToAdd)
+    {
+        score += scoreToAdd;
+        scoreText.text = "Score: " + score;
+    }
+
+    public void GameOver()
+    {
+        gameOverText.gameObject.SetActive(true);
+        restartButton.gameObject.SetActive(true);
+        isGameActive = false;
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
