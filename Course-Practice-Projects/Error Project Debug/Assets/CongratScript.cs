dd087987de0cb03f491d93b51ddcd75e493fea74
Abu Bakar Tahir <ch.abu.bakar.tahir.zain@gmail.com>
2024-01-07 02:37:19 +0500

    Error Project Debug
    Part of Checkpoint of Mission 2 of Unity Programmer Pathway in which i was given a Error Project which i have to debug. This is my Solution.

---
+++ Course-Practice-Projects/Error Project Debug/Assets/CongratScript.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CongratScript : MonoBehaviour
+{
+    public TextMesh Text;
+    public ParticleSystem SparksParticles;
+    
+    [SerializeField] List<string> TextToDisplay;
+
+    [SerializeField] float RotatingSpeed;
+    [SerializeField] float TimeToNextText;
+
+    [SerializeField] int CurrentText;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        TimeToNextText = 0.0f;
+        CurrentText = 0;
+        
+        RotatingSpeed = 1.0f;
+
+        TextToDisplay.Add("Congratulation");
+        TextToDisplay.Add("All Errors Fixed");
+
+        Text.text = TextToDisplay[CurrentText];
+        
+        SparksParticles.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TimeToNextText += Time.deltaTime;
+
+        if (TimeToNextText > 1.5f)
+        {
+            TimeToNextText = 0.0f;
+            
+            CurrentText++;
+            if (CurrentText >= TextToDisplay.Count)
+            {
+                CurrentText = 0;
+            }
+            Text.text = TextToDisplay[CurrentText];
+        }
+    }
+}
